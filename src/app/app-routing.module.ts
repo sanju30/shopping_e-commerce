ae565f9cce11873a4656ab86eac8f5ff17cff696
sanjana.k <sanjana.k@pickles.com.au>
2021-10-09 20:56:18 +0530

    app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { CheckoutPageComponent } from './components/product-checkout/checkout-page/checkout-page.component';
-import { MainPageComponent } from './components/shared/main-page/main-page/main-page.component';
-
-const routes: Routes = [
-  { path: 'shopping', component: MainPageComponent },
-  { path: '**', component: MainPageComponent },  // Wildcard route for a 404 page
-  { path: '',   redirectTo: '/MainPageComponent', pathMatch: 'full' }
-];
-
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes), CommonModule],
+  imports: [CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { NavComponent } from './components/shared/nav/nav.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
 import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
 import { CartComponent } from './components/shopping-cart/cart/cart.component';
 import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
 import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
 import { AddToCartComponent } from './components/shared/nav/add-to-cart/add-to-cart.component';
-import { CheckoutPageComponent } from './components/product-checkout/checkout-page/checkout-page.component';
-
-
+import { AppRoutingModule } from './app-routing.module';
+import { MainPageComponent } from './components/main-page/main-page/main-page.component';
+import { CheckoutFormComponent } from './components/shopping-cart/checkout-form/checkout-form.component';
 
 @NgModule({
   declarations: [
@@ -23,16 +21,18 @@ import { CheckoutPageComponent } from './components/product-checkout/checkout-pa
     NavComponent,
     FooterComponent,
     ShoppingCartComponent,
-    FiltersComponent,
     ProductListComponent,
     CartComponent,
     CartItemComponent,
     ProductItemComponent,
     AddToCartComponent,
-    CheckoutPageComponent,
+    MainPageComponent,
+    CheckoutFormComponent,
+
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
