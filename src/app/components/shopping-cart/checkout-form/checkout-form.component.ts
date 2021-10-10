99ab81ce84231d05198bd919387eede3d2781df5
sanjana.k <sanjana.k@pickles.com.au>
2021-10-10 13:20:18 +0530

    remove css

--- src/app/components/shopping-cart/checkout-form/checkout-form.component.ts
+++ src/app/components/shopping-cart/checkout-form/checkout-form.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkout-form',
-  templateUrl: './checkout-form.component.html',
-  styleUrls: ['./checkout-form.component.css']
+  templateUrl: './checkout-form.component.html'
 })
 export class CheckoutFormComponent implements OnInit {
 
